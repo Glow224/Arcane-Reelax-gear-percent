f1955e11604afde3ce27542f592cd0efc6a3176a
GOLDENGLOWGLOW <112446849+GOLDENGLOWGLOW@users.noreply.github.com>
2026-08-19 06:27:46 +0800

    Update Arcane-Reelax-gear-percent.js

--- Arcane-Reelax-gear-percent.js
+++ Arcane-Reelax-gear-percent.js
@@ -9,6 +9,8 @@
 // @grant        none
 // @run-at       document-end
 // @noframes
+// @downloadURL https://raw.githubusercontent.com/GOLDENGLOWGLOW/Arcane-Reelax-gear-percent/refs/heads/main/Arcane-Reelax-gear-percent.js
+// @updateURL https://raw.githubusercontent.com/GOLDENGLOWGLOW/Arcane-Reelax-gear-percent/refs/heads/main/Arcane-Reelax-gear-percent.js
 // ==/UserScript==
 
 (function() {
